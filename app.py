657fbd8f294952d723b60a95ba080b121b12404e
Reem Vahora <vahora.reem@gmail.com>
2025-05-28 10:52:12 +1000

    added pie chart; added button to jump between chat and results; added more info on prompts to add a human touch to the questions asked

--- app.py
+++ app.py
@@ -36,7 +36,6 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-
 def display_chat_history():
     for i, msg in enumerate(st.session_state.chat_history[2:]):
         if "✓" in msg["content"]:
