5a9ee66297a479369b2b42852f5bb24baced29f7
Reem Vahora <vahora.reem@gmail.com>
2025-06-24 08:07:43 +1000

    css efficiency ('inheritance')

--- src/Home.py
+++ src/Home.py
@@ -7,8 +7,10 @@ st.set_page_config(page_title="KeshCare", page_icon="🪷", layout="centered")
 
 initSessionState()
 
-with open("src/styles-title.css") as f:
-    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+with open("src/styles.css") as base, open("src/styles-title.css") as title_page:
+    st.markdown(
+        f"<style>{base.read()}\n{title_page.read()}</style>", 
+                unsafe_allow_html=True)
 
 st.markdown('<h1 class="custom-title">KeshCare</h1>', unsafe_allow_html=True)
 st.write(prompts.buildHomeInfo())
